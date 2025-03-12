a851ffdde323f4fc41872757c8389ba6e54e8fbf
jm@habit-streak.app <support@habit-streak.app>
2025-03-12 21:14:50 +0100

    add service worker

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto'
+        })
+    ]
+})
