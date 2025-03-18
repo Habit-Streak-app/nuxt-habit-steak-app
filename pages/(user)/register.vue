2dcb86975c18aec39e32b1367f6fbe2694c225da
jm@habit-streak.app <support@habit-streak.app>
2025-03-18 20:43:06 +0100

    add register page

---
+++ pages/(user)/register.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="flex justify-center">
+    <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+      <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
+      <form @submit.prevent="register">
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+            Email
+          </label>
+          <input
+            v-model="email"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+            Password
+          </label>
+          <input
+            v-model="password"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+        <div class="flex items-center justify-end">
+          <button
+            class="bg-blue-500  btn btn-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { usePocketBase } from '~/stores/pocketbase';
+import { useRouter } from 'vue-router';
+
+const email = ref('');
+const password = ref('');
+const pb = usePocketBase();
+const router = useRouter();
+
+onMounted(() => {
+	if (pb.authStore.isValid) {
+		router.push('/habits');
+	}
+});
+
+const register = async () => {
+	try {
+		await pb.collection('users').create({
+      email: email.value,
+      password: password.value,
+      passwordConfirm: password.value,
+    });
+    login();
+		router.push('/habits');
+	} catch (error) {
+		// TODO notify user about error
+		console.error(error);
+	}
+};
+
+const login = async () => {
+	try {
+		await pb.collection('users').authWithPassword(email.value, password.value);
+		router.push('/habits');
+	} catch (error) {
+		// TODO notify user about error
+		console.error(error);
+	}
+};
+</script>
