15c5097e422ce3313211088023eb238eb05ab89a
jm@habit-streak.app <support@habit-streak.app>
2025-03-21 21:38:01 +0100

    add primary for selected

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -9,9 +9,9 @@ const currentLocale = locale.value;
 <template>
   <div class="divider divider-primary block text-center font-bold">{{ $t('lang.change') }}</div>
   <div class="space-y-3">
-    <button class="btn btn-secondary w-full" @click="setLocale('en')">{{ $t('lang.en') }}</button>
-    <button class="btn btn-primary w-full" @click="setLocale('de')">{{ $t('lang.de') }}</button>
-    <button class="btn btn-primary w-full" @click="setLocale('fr')">{{ $t('lang.fr') }}</button>
+    <button class="btn w-full" :class="{'btn-primary' : currentLocale== 'en','btn-secondary' : currentLocale!= 'en'}" @click="setLocale('en')">{{ $t('lang.en') }}</button>
+    <button class="btn w-full" :class="{'btn-primary' : currentLocale== 'de','btn-secondary' : currentLocale!= 'de'}" @click="setLocale('de')">{{ $t('lang.de') }}</button>
+    <button class="btn w-full" :class="{'btn-primary' : currentLocale== 'fr','btn-secondary' : currentLocale!= 'fr'}" @click="setLocale('fr')">{{ $t('lang.fr') }}</button>
     <p class="prose w-full bg-white px-3 py-3 text-black text-center">
       {{ $t('lang.selected') }}:
       {{ $t('lang.' + currentLocale) }}
